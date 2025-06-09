63236ec8d9e1e49570bd40c66a003f618a05a093
Maxim Boldyrev <bmax0409@yandex.ru>
2025-06-09 13:46:00 +0300

    Final commit

--- src/main/java/ru/practicum/pages/MainPage.java
+++ src/main/java/ru/practicum/pages/MainPage.java
@@ -20,22 +20,6 @@ public class MainPage {
     private static final By buttonOnTheTop = By.cssSelector(".Button_Button__ra12g");
     private static final By middleOrderButton = By.cssSelector(".Button_Middle__1CSJM");
     private static final By finalQuestion = By.id("accordion__heading-7");
-    public static final String firstQuestion = "accordion__heading-0";
-    public static final String secondQuestion = "accordion__heading-1";
-    public static final String thirdQuestion = "accordion__heading-2";
-    public static final String forthQuestion = "accordion__heading-3";
-    public static final String fifthQuestion = "accordion__heading-4";
-    public static final String sixthQuestion = "accordion__heading-5";
-    public static final String seventhQuestion = "accordion__heading-6";
-    public static final String eighthQuestion = "accordion__heading-7";
-    public static final String firstAnswer = "#accordion__panel-0 > p";
-    public static final String secondAnswer = "#accordion__panel-1 > p";
-    public static final String thirdAnswer = "#accordion__panel-2 > p";
-    public static final String fourthAnswer = "#accordion__panel-3 > p";
-    public static final String fifthAnswer = "#accordion__panel-4 > p";
-    public static final String sixthAnswer = "#accordion__panel-5 > p";
-    public static final String seventhAnswer = "#accordion__panel-6 > p";
-    public static final String eighthAnswer = "#accordion__panel-7 > p";
 
     // Конструктор
     public MainPage(WebDriver driver) {
@@ -43,38 +27,40 @@ public class MainPage {
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(EnvConfig.EXPLICITY_TIMEOUT));
     }
 
-    // Открытие главной страницы
     public void openMainPage() {
         driver.get(EnvConfig.BASE_URL);
     }
 
-    // Нажатие кнопки "Заказать" вверху страницы
     public void clickOrderButtonOnTheTopOfThePage() {
         wait.until(ExpectedConditions.elementToBeClickable(buttonOnTheTop)).click();
     }
 
-    // Прокрутка вниз и нажатие средней кнопки заказа
     public void scrollDownAndClickTheMiddleButton() {
         WebElement middleButton = wait.until(ExpectedConditions.visibilityOfElementLocated(middleOrderButton));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", middleButton);
         middleButton.click();
     }
 
-    // Проверка текста ответа по селектору
     public void checkTheAnswer(String answerSelector, String expectedAnswer) {
         WebElement answer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(answerSelector)));
         String actualText = answer.getText();
         assertEquals(expectedAnswer, actualText);
     }
 
-    // Нажатие на вопрос по id
-    public void clickTheQuestion(String questionId) {
-        WebElement question = wait.until(ExpectedConditions.elementToBeClickable(By.id(questionId)));
-        question.click();
+    public void clickQuestionByIndex(int index) {
+        String questionId = getQuestionIdByIndex(index);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id(questionId))).click();
     }
 
-    // Прокрутка до последнего вопроса
-    public void scrollToTheBottomOfThePage() {
+    public String getAnswerSelectorByIndex(int index) {
+        return "#accordion__panel-" + index + " > p";
+    }
+
+    public String getQuestionIdByIndex(int index) {
+        return "accordion__heading-" + index;
+    }
+
+    public void scrollToLastQuestion() {
         WebElement lastQuestion = wait.until(ExpectedConditions.visibilityOfElementLocated(finalQuestion));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", lastQuestion);
     }
--- src/test/java/ru/practicum/tests/ScooterMainPageFAQTest.java
+++ src/test/java/ru/practicum/tests/ScooterMainPageFAQTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import ru.practicum.pages.MainPage;
-import ru.practicum.util.MainPageFAQData;
 
 @RunWith(Parameterized.class)
 public class ScooterMainPageFAQTest {
@@ -17,37 +16,43 @@ public class ScooterMainPageFAQTest {
     private WebDriver driver;
     private MainPage mainPage;
 
-    // Параметры теста: id вопроса, селектор ответа, ожидаемый ответ
-    private final String questionId;
-    private final String answerSelector;
+    // Параметры теста: индекс вопроса, ожидаемый ответ
+    private final int questionIndex;
     private final String expectedAnswer;
 
-    public ScooterMainPageFAQTest(String questionId, String answerSelector, String expectedAnswer) {
-        this.questionId = questionId;
-        this.answerSelector = answerSelector;
+    public ScooterMainPageFAQTest(int questionIndex, String expectedAnswer) {
+        this.questionIndex = questionIndex;
         this.expectedAnswer = expectedAnswer;
     }
 
     @Parameterized.Parameters(name = "Вопрос: {0}")
     public static Object[][] data() {
-        return MainPageFAQData.FAQ_DATA; // Используем внешние данные
+        return new Object[][]{
+                {0, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {1, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {4, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {6, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {7, "Да, обязательно. Всем самокатов! И Москве, и Московской области."}
+        };
     }
 
     @Test
     public void checkDropDownList() throws InterruptedException {
-        // Получение драйвера из фабрики для каждого теста
         driver = driverFactory.getDriver();
 
-        // Создаем страницу с текущим драйвером
         mainPage = new MainPage(driver);
 
         mainPage.openMainPage();
-        mainPage.scrollToTheBottomOfThePage();
-        mainPage.clickTheQuestion(questionId);
+        mainPage.scrollToLastQuestion();
+        mainPage.clickQuestionByIndex(questionIndex);
+        String answerSelector = mainPage.getAnswerSelectorByIndex(questionIndex);
         mainPage.checkTheAnswer(answerSelector, expectedAnswer);
+
     }
 }
-
 
 
 
